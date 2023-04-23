207624990a2eb83c1e61791a3d251f49efaff121
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-04-23 17:03:41 +0300

    feat(dashboard): create page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
+import Layout from "@/components/Layout";
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
   const { data: session } = useSession()
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button className="bg-white p-2 px-4 rounded-lg" onClick={() => signIn('google')}>Login with Google</button>
-        </div>
+  
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-200 text-black gap-1 rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="user image" className="rounded-full h-6 w-6 ml-2" />
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
       </div>
-    )
-  }
-  return (
-   <div>logged in {session.user.email}</div>
-  )
+    </div>
+  </Layout>
 }
- 
+  
