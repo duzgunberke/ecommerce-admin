5b49e4d50401f5662332815ea809f23195d553b3
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-04-25 02:24:35 +0300

    feat(Product): Product opertions done

--- components/ProductsForm.js
+++ components/ProductsForm.js
@@ -2,17 +2,22 @@ import Layout from "@/components/Layout";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "./Spinner";
+import { ReactSortable } from "react-sortablejs";
 
 export default function ProductForm(
     {   _id,
         title: existingTitle,
         description: existingDescription, 
         price: existingPrice,
+        images: existingImages
     }){
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
     const [goToProducts, setGoToProducts] = useState(false);
+    const [isUploading, setIsUploading] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev){
@@ -20,7 +25,8 @@ export default function ProductForm(
         const data ={
             title,
             description,
-            price
+            price,
+            images
         }
         if(_id){
             // Update Product
@@ -36,9 +42,31 @@ export default function ProductForm(
     if(goToProducts){
      router.push('/products');
     }
+
+    async function uploadImages(ev){
+        const files = ev.target?.files;
+        if(files?.length > 0){
+            setIsUploading(true);
+            const data = new FormData();
+            for (const file of files){
+                data.append('file',file);
+            }
+            const res = await axios.post('/api/upload',data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links];
+            });
+            setIsUploading(false);
+       }
+    }
+
+    function updateImagesOrder(images){
+        setImages(images);
+    }
+
     return(
         
-            <form onSubmit={createProduct}>
+            <form onSubmit={saveProduct}>
+                {/* Product Name */}
                 <label>Product Name</label>
                 <input 
                     type="text" 
@@ -46,14 +74,47 @@ export default function ProductForm(
                     value={title} 
                     onChange={event => setTitle(event.target.value)} />
 
+                {/* Product Images */}
+                <label>Product Photos</label>
+                <div className="mb-2 flex flex-wrap gap-1">
+                    <ReactSortable 
+                        list={images} 
+                        setList={updateImagesOrder}
+                        className="flex flex-wrap gap-1">
+                        {!!images?.length > 0 && images.map(link =>(
+                            <div key={link}  className="h-24 bg-white p-4 shadow-sm rounded-lg border border-gray-200">
+                                <img src={link} className="rounded-lg" />
+                            </div>
+                        ))}
+                    </ReactSortable>
+                    {isUploading && (
+                        <div className="h-24  p-1 flex items-center">
+                            <Spinner />
+                        </div>
+                    )}
+                    <label className="w-24 h-24 cursor-pointer gap-1 bg-gray-200 rounded-lg text-gray-500 text-center text-sm flex flex-col items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                        </svg>
+                        <div>
+                            Upload
+                        </div>
+                        <input 
+                            type="file" 
+                            className="hidden" 
+                            onChange={uploadImages}
+                            />
+                    </label>
+                </div>
 
+                {/* Product Description */}
                 <label>Product Description</label>
                 <textarea 
                     placeholder="Product Description" 
                     value={description} 
                     onChange={event => setDescription(event.target.value)} />                      
 
-
+                {/* Product Price */}
                 <label>Product Price</label>
                 <input 
                     type="number" 
@@ -61,7 +122,7 @@ export default function ProductForm(
                     value={price} 
                     onChange={event => setPrice(event.target.value)} /> 
 
-                <button type="submit" className="btn-primary">Add Product</button>
+                <button type="submit" className="btn-primary">Save Product</button>
             </form>
     )
 }
--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -1,18 +1,48 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function DeleteProductPage(){
     const router = useRouter();
+    const [productInfo, setProductInfo] = useState();
+    const {id} = router.query;
+
+    useEffect(()=>{
+        if(!id) return;
+
+        axios.get('/api/products?id='+id).then(response =>{
+            setProductInfo(response.data);
+        });
+    },[id])
 
     function goBack(){
         router.push('/products');
     }
-    
+
+    async function deleteProduct(){
+        await axios.delete('/api/products?id='+id);
+        goBack();
+    }
+
     return(
         <Layout>
-            Do you really wanna delete product X ?
-            <button>YES</button>
-            <button onClick={goBack}>NO</button>
+            <h1 className="text-center">
+                Do you really wanna delete product&nbsp;"{productInfo?.title}" ?
+            </h1>
+            <div className="flex gap-2 justify-center">
+                <button 
+                    onClick={deleteProduct}
+                    className="btn-red">
+                        YES
+                </button>
+
+                <button 
+                    className="btn-default" 
+                    onClick={goBack}>
+                        NO
+                </button>
+            </div>
         </Layout>
     )
 }
